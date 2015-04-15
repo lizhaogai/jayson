47aace4d86912f08c1389d173381816d4bb724fc
yifeng <493788939lizhaogai@163.com>
2015-04-15 19:35:21 +0800

    c

--- test/mqtt.server.js
+++ test/mqtt.server.js
@@ -16,14 +16,3 @@ var mqtt = server.mqtt({
     topic: "$device/1234567890/channel/:protocol",
     bus: bus
 });
-
-// "https" will be an instance of require('https').Server
-//var https = server.https({
-//    //cert: require('fs').readFileSync('cert.pem'),
-//    //key require('fs').readFileSync('key.pem')
-//});
-
-//mqtt.connect({
-//    host: "localhost",
-//    topic: "$device/1234567890/channel/:protocol/:method"
-//});
